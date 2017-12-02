65bb5de091aa0f9016a9dd0778cf95773933911b
pnasoftware <pnasoftware@outlook.com>
2017-12-02 05:22:29 +0100

    DBManager - update

--- Hackaton-Wpf/Hackaton-Wpf/DataBase/BotProfile.cs
+++ Hackaton-Wpf/Hackaton-Wpf/DataBase/BotProfile.cs
@@ -8,13 +8,12 @@ namespace Hackathon
 {
     public class BotProfile
     {
-        /// <summary>
-        /// Tags for searching e.g. news
-        /// </summary>
-        public List<string> SearchTags { get; set; }
-        /// protectedsummary>
-        /// Dictionary of words for autocorrect
-        /// </summary>
-        public List<string> DictionaryOfWords { get; set; }
+
+        public string botName { get; }
+
+        public BotProfile()
+        {
+            botName = "Memyslaw";
+        }
     }
 }
